1f0198a246b3c998543343c66b342ec130b69d3b
Stephen Elliott <stephen@bealine.co.uk>
2019-02-01 14:16:22 +0000

    Add first test

---
+++ src/test/java/uk/gov/dft/bluebadge/service/payment/PaymentServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package uk.gov.dft.bluebadge.service.payment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class PaymentServiceApplicationTest extends ApplicationContextTests{
+  @Test
+  public void contextLoads() {
+  }
+}
