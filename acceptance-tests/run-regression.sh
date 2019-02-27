5bd638dcd5ba1d4f63728111090bfef62338a4e8
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-02-27 11:03:45 +0000

    Making it executable

--- acceptance-tests/run-regression.sh
+++ acceptance-tests/run-regression.sh
@@ -2,4 +2,5 @@
 
 curl -s -o run-regression-script.sh -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -O -L https://raw.githubusercontent.com/uk-gov-dft/shell-scripts/master/run-regression.sh
 
+chmod +x run-regression-script.sh
 ./run-regression-script.sh
